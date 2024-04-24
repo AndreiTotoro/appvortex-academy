b9fc1c000c61c62ef7339ec58ffc6dfcc78b020b
andrei <andreitotoro@gmail.com>
2024-04-24 22:08:01 +0300

    Create the footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t">
+      <div
+        className="flex-center wrapper flex-between flex 
+      flex-col gap-4 p-5 text-center sm:flex-row"
+      >
+        <Link href="/">Vortex Events</Link>
+        <p>2024 Vortex Events. All Rights reserved.</p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
