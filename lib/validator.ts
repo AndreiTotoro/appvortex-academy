799a989f226f62fd47469f4b59fbaef6b93e45d5
andrei <andreitotoro@gmail.com>
2024-04-25 17:03:48 +0300

    Create the zod form schema validation

---
+++ lib/validator.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+export const eventFormSchema = z.object({
+  title: z
+    .string()
+    .min(3, "Title must be at least 3 characters")
+    .max(100, "Description must be at most 100 characters"),
+  locations: z
+    .string()
+    .min(3, "Location must be at least 3 characters")
+    .max(100, "Location must be at most 100 characters"),
+  imageUrl: z.string().url("Invalid URL"),
+  startDateTime: z.date(),
+  endDateTime: z.date(),
+  categoryId: z.string(),
+  price: z.string(),
+  isFree: z.boolean(),
+  url: z.string().url("Invalid URL"),
+});
