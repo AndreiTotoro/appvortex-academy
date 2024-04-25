cd5fe4e6b0c7f7415a53b27e20052762b16e0cf2
andrei <andreitotoro@gmail.com>
2024-04-25 18:47:31 +0300

    Add description possibility

--- lib/validator.ts
+++ lib/validator.ts
@@ -10,6 +10,10 @@ export const eventFormSchema = z.object({
     .min(3, "Location must be at least 3 characters")
     .max(100, "Location must be at most 100 characters"),
   imageUrl: z.string().url("Invalid URL"),
+  description: z
+    .string()
+    .min(3, "Description must be at least 3 characters")
+    .max(500, "Description must be at most 500 characters"),
   startDateTime: z.date(),
   endDateTime: z.date(),
   categoryId: z.string(),
